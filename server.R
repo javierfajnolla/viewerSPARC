737941a8e1f0f16c86e90310ff84949c0572404c
javierfajnolla <javierfajnolla@gmail.com>
2019-03-28 00:49:40 +0100

    Commit initial files

---
+++ server.R
@@ -0,0 +1,173 @@
+library(tidyverse)
+library(leaflet)
+library(RColorBrewer)
+library(scales)
+library(lattice)
+library(dplyr)
+library(plotly)
+
+# Leaflet bindings are a bit slow; for now we'll just sample to compensate
+# set.seed(100)
+
+leaflet(options = leafletOptions(control = "topright")) %>%
+  addTiles()
+
+# Reactive values object storage
+rvs <- reactiveValues()
+
+function(input, output, session) {
+  
+  ## Interactive Map ###########################################
+  output$map <- renderLeaflet({
+    # leaflet(TAC_border) %>% 
+    leaflet(options = leafletOptions(zoomControl = FALSE)) %>%
+        htmlwidgets::onRender("function(el, x) {    
+                                L.control.zoom({ position: 'topright' }).addTo(this)
+                              }") %>% # This code just moves the zoom buttons to the right
+      addTiles() %>%
+      fitBounds(lng1 = (extent(solution)[1] + extent(solution)[1]),    # zoom to raster extent
+                lat1 = extent(solution)[3],
+                lng2 = extent(solution)[2],
+                lat2 = extent(solution)[4]) #%>%
+  })
+  
+  # Create map proxy to make further changes to existing map
+  map <- leafletProxy("map") %>% 
+    clearControls()  # Can't make the legend to disappear...
+  
+  # React to modification in Threshold 1
+  observeEvent(input$thr1, {
+    glue::glue("#> thr1 has changed - adjusting thr2") %>% print
+    
+    # Select posible values in thr2 corresponding to the change in thr1
+    rvs$possibles_table_thr2 <- tbl %>%
+      filter(abs(perc_pixels - input$thr1) == min(abs(perc_pixels - input$thr1)))
+    rvs$possibles_values_thr2 <- rvs$possibles_table_thr2 %>% 
+      pull(prop_carbon_strg)
+    
+    # Check in the current value in thr2 is among the possible values
+    needs_change <- !(input$thr2 %in% rvs$possibles_values_thr2)
+    
+    # If thr2 has to change, trigger the change and selection of new values
+    if(needs_change){
+      # Check if there is only one possible value or there are more
+      if(length(rvs$possibles_values_thr2) > 1){
+        # If more than one, select the first
+        rvs$possibles_values_thr2 <- rvs$possibles_values_thr2[1]
+        rvs$possibles_table_thr2 <- rvs$possibles_table_thr2[1,]
+      }
+      
+      # Update the slider
+      updateSliderInput(session = session, inputId = "thr2",
+                        value = rvs$possibles_values_thr2)
+      
+    }
+    
+    # save the new value to reclasify the raster
+    rvs$new_thr <- rvs$possibles_table_thr2 %>% pull(cutoff_sol)
+  })
+  
+  observeEvent(input$thr2, {
+    glue::glue("#> thr2 has changed - adjusting thr1") %>% print
+    
+    # Select posible values in thr2 corresponding to the change in thr1
+    rvs$possibles_table_thr1 <- tbl %>%
+      filter(abs(prop_carbon_strg - input$thr2) == min(abs(prop_carbon_strg - input$thr2)))
+    rvs$possibles_values_thr1 <- rvs$possibles_table_thr1 %>% 
+      pull(perc_pixels)
+    
+    # Check in the current value in thr2 is among the possible values
+    needs_change <- !(input$thr1 %in% rvs$possibles_values_thr1)
+    
+    # If thr2 has to change, trigger the change and selection of new values
+    if(needs_change){
+      # Check if there is only one possible value or there are more
+      if(length(rvs$possibles_values_thr1) > 1){
+        # If more than one, select the first
+        rvs$possibles_values_thr1 <- rvs$possibles_values_thr1[1]
+        rvs$possibles_table_thr1 <- rvs$possibles_table_thr1[1,]
+      }
+      
+      # Update the slider
+      updateSliderInput(session = session, inputId = "thr1",
+                        value = rvs$possibles_values_thr1)
+      
+    }
+    
+    # save the new value to reclasify the raster
+    rvs$new_thr <- rvs$possibles_table_thr1 %>% pull(cutoff_sol)
+  })
+  
+  rvs$plot_solution <- solution
+  
+  observeEvent(input$speed, {
+    if (input$speed == "Slow - max detail"){
+      glue::glue("Now it is in slow") %>% print
+      rvs$plot_solution <- solution
+    } 
+    if (input$speed == "Medium - medium detail"){
+      glue::glue("Now it is in medium") %>% print
+      rvs$plot_solution <- solution_2
+    } 
+    if (input$speed == "Fast - less detail"){
+      glue::glue("Now it is in fassssst") %>% print
+      rvs$plot_solution <- solution_3
+    }
+  })  
+   
+  observeEvent(rvs$new_thr, {
+    # Threshold and reclassify
+    # sel_thr <- rvs$sel_row %>% pull(cutoff_sol)
+    glue::glue("{rvs$new_thr}") %>% print
+    
+    sol_rcl <- rvs$plot_solution %>% 
+      reclassify(tibble(from = c(0, rvs$new_thr),
+                        to = c(rvs$new_thr, max(values(rvs$plot_solution), na.rm = T)),
+                        becomes = c(NA, 1)))
+    
+    # Leaflet
+    pal_A <- colorFactor(
+      # palette = input$color_A,
+      palette = "Oranges",
+      levels = c(0.5, 1),
+      na.color = "transparent",
+      reverse = FALSE)
+    
+    map %>% 
+      clearControls() %>%    # Refreshes the legend
+      clearGroup("solutions_A") %>%
+      addRasterImage(sol_rcl,
+                     colors = pal_A,
+                     # opacity = input$opacity_A,
+                     group = "solutions_A") %>%
+      addLegend(pal = pal_A,
+                values = c(NA, 1),
+                group = "solutions_A",
+                layerId = "raster",
+                labFormat = labelFormat(transform = function(x) sort(x, decreasing = TRUE)),
+                position = "topright")
+  })
+  
+  ### Plotly graph
+  observeEvent(input$thr1, {
+    plot1 <- tbl %>% 
+      mutate(perc_pixels = perc_pixels,
+             prop_carbon_strg = prop_carbon_strg) %>% 
+      ggplot +
+      geom_area(aes(x = perc_pixels, y = prop_carbon_strg)) +
+      geom_area(data = tbl %>% 
+                  mutate(perc_pixels = perc_pixels,
+                         prop_carbon_strg = prop_carbon_strg) %>% 
+                  filter(perc_pixels <= input$thr1),
+                aes(x = perc_pixels, y = prop_carbon_strg),
+                fill = "#E45621") +
+      xlab("Proportion of study area protected") +
+      ylab("Proportion of carbon storage saved") +
+      theme_minimal()
+    
+    output$plotly <- renderPlotly(
+      plotly::ggplotly(plot1)
+    )
+  })
+  
+}
---
+++ ui.R
@@ -0,0 +1,106 @@
+library(leaflet)
+library(tidyverse)
+library(plotly)
+
+# Choices for drop-downs
+vars <- c(
+  "Is SuperZIP?" = "superzip",
+  "Centile score" = "centile",
+  "College education" = "college",
+  "Median income" = "income",
+  "Population" = "adultpop"
+)
+
+
+navbarPage(title = div(
+  div(
+    id = "img-id",
+    img(src = "coded_JF.png", width = 180)
+  ),
+  "SPARC VISUALIZER"
+),
+           id = "nav",
+           tabPanel("Interactive map",
+                    div(class = "outer",
+                        
+                        tags$head(
+                          # Include our custom CSS
+                          includeCSS("styles.css"),
+                          includeScript("gomap.js")
+                        ),
+                        
+                        # If not using custom CSS, set height of leafletOutput to a number instead of percent
+                        leafletOutput("map", width = "100%", height = "100%"),
+                        
+                        # Shiny versions prior to 0.11 should use class = "modal" instead.
+                        absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                      draggable = TRUE, top = 60, left = 10, right = "auto", bottom = "auto",
+                                      width = 400, height = "auto",
+                                      
+                                      hr(),
+                                      
+                                      img(src = "sparc_logo_thin.png", 
+                                          width = 250),
+                                          # style = "text-align: center;"),
+                                      
+                                      hr(),
+                                      
+                                      # checkboxInput("layA", label = "LAYER 1", value = T),
+                                      
+                                      # column(6, numericInput("thresholds_A", "Number Colors",
+                                      #              min = 2, max = 3, value = 2)),
+                                      # 
+                                      # column(6, selectInput("color_A", "Palette",
+                                      #                       c("Purple to yellow" = "viridis",
+                                      #                         "Yellow to black" = "magma",
+                                      #                         "Greens" = "Greens",
+                                      #                         "Blue to purple" = "BuPu",
+                                      #                         "Oranges" = "Oranges",
+                                      #                         "Blues" = "Blues",
+                                      #                         "Greys" = "Greys",
+                                      #                         "Reds" = "Reds",
+                                      #                         "Purples" = "Purples"),
+                                      #                       selected = "Oranges",
+                                      #                       width = 800)),
+                                      
+                                      sliderInput("thr1", "Solution threshold:",
+                                                  min = 0, max = 100, step = 1,
+                                                  value = 17),
+                                      
+                                      # hr(),
+                                      
+                                      sliderInput("thr2", "Carbon offset",
+                                                  min = 0, max = 100, step = 1,
+                                                  value = tbl %>% 
+                                                    filter(abs(perc_pixels - 17) == min(abs(perc_pixels - 17))) %>% 
+                                                    pull(prop_carbon_strg)),
+                                      
+                                      # hr(),
+                                      
+                                      radioButtons("speed", "Speed of refresh (affects detail)",
+                                                   choices = c("Slow", 
+                                                               "Medium",
+                                                               "Fast"),
+                                                   inline = TRUE,
+                                                   selected = "Slow - max detail"),
+                                      
+                                      plotlyOutput("plotly", 
+                                                   width = "90%"),
+                                      
+                                      hr()
+                                      
+                        )#,
+                        
+                        # tags$div(id="cite",
+                        #          'Coded by ', tags$em('Javier Fajardo'))
+                    )
+           ),
+
+           tabPanel("About",
+                    fluidRow(
+                      column(3, 
+                             h4("A description of the research and a link to the paper will appear here")
+                             )
+                      )
+                    )
+)
